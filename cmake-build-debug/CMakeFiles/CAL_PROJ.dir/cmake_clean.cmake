1bb868a2249b985c33198d3c1e1880b14e0bd509
Leonor <leonor.email@gmail.com>
2020-05-05 18:06:06 +0100

    added stuff

---
+++ cmake-build-debug/CMakeFiles/CAL_PROJ.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CAL_PROJ.dir/main.cpp.obj"
+  "CAL_PROJ.pdb"
+  "CAL_PROJ.exe"
+  "CAL_PROJ.exe.manifest"
+  "libCAL_PROJ.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CAL_PROJ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Person.h
@@ -0,0 +1,11 @@
+//
+// Created by user on 05/05/2020.
+//
+
+#ifndef CAL_PROJ_PERSON_H
+#define CAL_PROJ_PERSON_H
+
+
+
+
+#endif //CAL_PROJ_PERSON_H
