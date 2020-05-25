43635a57d1d174001993430bf6d61ea58864038d
Leonor <leonor.email@gmail.com>
2020-05-25 03:04:26 +0100

    more comments added

--- MeetupRider/src/FloydWarshall.h
+++ MeetupRider/src/FloydWarshall.h
@@ -7,12 +7,17 @@
 
 #include "NewGraph.h"
 #include <iostream>
+
+//escreve a matriz das distâncias no ficheiro
 void writeDistanceMatrix(double **matrix, string text_file, int size);
 
+//lê a matriz das distâncias do ficheiro
 double ** readDistanceMatrix(string text_file);
 
+//escreve a matriz do caminho no ficheiro
 void writePathMatrix(int **matrix, string text_file, int size);
 
+// le a matriz do caminho do ficheiro
 int ** readPathMatrix(string text_file);
 
 
