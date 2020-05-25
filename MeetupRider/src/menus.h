bbd36f7c726cd49fa3a97553cdd3d6e3abd9bb7b
Leonor <leonor.email@gmail.com>
2020-05-25 04:14:47 +0100

    added more comments

--- MeetupRider/src/menus.h
+++ MeetupRider/src/menus.h
@@ -13,20 +13,31 @@
 using namespace std;
 
 
+//menu em que se escolhe se se é um passageiro ou um condutor
 int main_menu(Base base);
 
+//menu em que se escolhe se se faz sign up ou sign in
 int next_menu(Base base, string type);
 
+//menu em que o utilizador faz um pedido de boleia ou oferece boleia
 void request_menu(Base *base,int id);
 
+//menu de escolha de mapa
 void chooseCity(Base *base);
 
+//menu de escolha de algoritmo a ser utilizado
 void chooseAlgorithm(Base * base,int id);
+
+//menu que da a opção de adicionar pedidos, ver o mapa, correr o algoritmo (processando os pedidos já existentes) e ver as viagens já realizadas
 void optionMenu(Base *base,int id);
+
+//função de apoio ao input de tempos
 Time *readTime(string timeType);
 
+//menu de visualização das viagens já realizadas
 void viewJourneys(Base *base);
 
+//menu onde se dá a escolha de fazer o processamento de dados no momento ou usar os preprocessados
 void floydwarshallMenu(Base*base,int id);
 
 #endif //MEETUPRIDER_MENUS_H
